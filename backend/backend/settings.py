ebf4163f6ed42e6f0ffba0c0007d972191052509
mishgun <uno2003@mail.ru>
2023-11-01 18:52:19 +0000

    first review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,10 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['84.252.139.255', '127.0.0.1', 'localhost', 'uno2003-taski.hopto.org']
 
 # Application definition
 
@@ -103,7 +102,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
